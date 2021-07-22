47db21743ec5facfa11424975e60b9596d3524c4
João Manito <joaomanito1@gmail.com>
2021-07-22 15:47:17 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-%matplotlib qt
+%matplotlib tk
 import matplotlib.pyplot as plt
 import scipy
 import numpy as np
@@ -29,23 +29,50 @@ sampFreq = 5 #In Hertz
 perigeeTime = 0
 sma = 6000000
 ecc = 0
+i = math.radians(55)
+Omega = math.radians(35)
+w0 = math.radians(60)
 
 x = np.zeros([3600*8*sampFreq])
 y = np.zeros([3600*8*sampFreq])
 z = np.zeros([3600*8*sampFreq])
 t = np.zeros([3600*8*sampFreq])
 
-for i in range(0,3600*8*sampFreq):
-    time = i/sampFreq
+for niter in range(0,3600*8*sampFreq):
+    time = niter/sampFreq
     trueAnom = orbitalMechanicsRoutines.computeTrueAnomaly(perigeeTime,time,sma,ecc)
     r = orbitalMechanicsRoutines.computeRadius(sma,ecc,trueAnom)
     
+    x_IRF = r*math.cos(trueAnom)
+    y_IRF = r*math.sin(trueAnom)
+    z_IRF = 0
+    
+    r_orbital = np.transpose(np.matrix([x_IRF,y_IRF,z_IRF]))
     
     
-    x[i] = r*math.cos(trueAnom)
-    y[i] = r*math.sin(trueAnom)
-    z[i] = 0
-    t[i] = time
+    #Define the rotation matrices from the orbital reference frame to the ECEF frame
+    rotX = np.array([[1,0,0],
+                     [0,math.cos(-i),math.sin(-i)],
+                     [0,-math.sin(-i),math.cos(-i)]])
+
+    rotZ = np.array([[math.cos(-Omega),math.sin(-Omega),0],
+                     [-math.sin(-Omega),math.cos(-Omega),0],
+                     [0,0,1]])
+    
+    rotZ2 = np.array([[math.cos(-w0),math.sin(-w0),0],
+                      [-math.sin(-w0),math.cos(-w0),0],
+                      [0,0,1]])
+    
+    #Use rotation matrices to obtain the satellite position in an ECEF frame 
+    r_ECEF = np.matmul(rotZ,np.matmul(rotX,np.matmul(rotZ2,r_orbital)))
+    
+    x[niter] = r_ECEF[0]
+    y[niter] = r_ECEF[1]
+    z[niter] = r_ECEF[2]
+    t[niter] = time
+    
+    if time == 10:
+        print("HERE!")
 
 
 t = np.arange(0, 3600*8*sampFreq, 1)
@@ -69,6 +96,9 @@ fig1, ax1 = plt.subplots()
 ax1.plot(t, x)
 fig2, ax2 = plt.subplots()
 ax2.plot(t, y)
+fig3, ax3 = plt.subplots()
+ax3.plot(t, z)
 ax1.grid()
 ax2.grid()
+ax3.grid()
 plt.show()
