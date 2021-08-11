c232848a047edf33efc398ed926d54ae68e9e9bf
João Manito <joaomanito1@gmail.com>
2021-08-11 17:11:59 +0100

    Create gnssSimulationRoutines.py

---
+++ src/gnssSimulationRoutines.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+gnssSimulationRoutines.py
+
+This file contains the functions required for simulating the GNSS signal propagation and reception
+"""
+
+def computeLOS():
